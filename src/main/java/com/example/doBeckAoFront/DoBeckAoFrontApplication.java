3eddea5da80eda2a4d06749a75e1d1c0dd3ccaec
IsraelFJ <israel.jesus@ba.estudante.senai.br>
2025-04-24 18:40:01 -0300

    Primeiro commit

---
+++ src/main/java/com/example/doBeckAoFront/DoBeckAoFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.doBeckAoFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoBeckAoFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoBeckAoFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doBeckAoFront/DoBeckAoFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doBeckAoFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoBeckAoFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
