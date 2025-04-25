a6392d43d5b37545a1e24d3fb484ca5187f0ef16
IsraelFJ <israel.jesus@ba.estudante.senai.br>
2025-04-24 21:42:42 -0300

    commit

--- src/main/java/com/example/doBeckAoFront/controller/UsuarioController.java
+++ src/main/java/com/example/doBeckAoFront/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")// aceita coexão de aplicaçoes externas
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
--- src/main/java/com/example/doBeckAoFront/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/doBeckAoFront/exception/GlobalExceptionHandler.java
@@ -5,21 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handlerRuntimeException(RuntimeException erro){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Mensagem", erro.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Mensagem", "Ação invalida "));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException erro){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Mensagem", erro.getFieldErrors().get(0).getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Mensagem", "Ação não reconhecida favor tente novamente"));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
@@ -29,7 +29,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<Map<String, Object>> NoResourceFoundException(NoResourceFoundException erro){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensagem", "URL invalida ou incompleta, por favor corrgir e tentar novamente"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensagem", "URL invalida ou incompleta, por favor corrigir e tentar novamente"));
     }
 
 }
